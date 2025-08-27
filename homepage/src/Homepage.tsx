3568c71ef4bf6c36c19e22bc773159497d5e92ac
eeso <eesowrk@gmail.com>
2025-08-27 19:31:16 -0400

    make the homepage into a dynamic app

---
+++ homepage/src/Homepage.tsx
@@ -0,0 +1,13 @@
+import './Homepage.css'
+
+export default function Homepage() {
+
+    return (
+        <>
+            <div>
+                <p>we on the homepage</p>
+            </div>
+        </>
+    )
+}
+
