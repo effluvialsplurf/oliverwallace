504458c51b67deb93a417f4847006c9d3fb5ec5e
eeso <eesowrk@gmail.com>
2025-08-27 22:01:23 -0400

    had two of the projects nav paths switched around

--- homepage/src/components/HomepageNavCol.tsx
+++ homepage/src/components/HomepageNavCol.tsx
@@ -3,8 +3,8 @@ export default function HompageNavCol() {
     return (
         <>
             <ul className="flex flex-col self-center w-3/10 text-xl">
-                <li className="my-5"><a href="/oliverwallace/tf-image-recognition">First TF Project (guessing double a number)</a></li>
-                <li className="my-5"><a href="/oliverwallace/tf-number-predictor">Second TF Project (image interpolation)</a></li>
+                <li className="my-5"><a href="/oliverwallace/tf-number-predictor">First TF Project (guessing double a number)</a></li>
+                <li className="my-5"><a href="/oliverwallace/tf-image-recognition">Second TF Project (image interpolation)</a></li>
                 <li className="my-5"><a href="/oliverwallace/threeJS-practice">Practicing ThreeJS</a></li>
             </ul>
         </>
