dc941155ebf17e13036fbfbb687980c73e491c1d
eeso <eesowrk@gmail.com>
2025-08-27 21:18:12 -0400

    forgor I added a file

--- homepage/src/main.tsx
+++ homepage/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomepageBio from './HomepageBio.tsx'
+import HomepageBio from './components/HomepageBio.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
